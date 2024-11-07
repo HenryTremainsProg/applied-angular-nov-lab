f4ae0f9f72023fad26da49af327434bee9b11206
HT <Henry_Tremains@progressive.com>
2024-11-07 13:57:44 -0500

    add ui

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,15 +1,17 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-counter',
-    standalone: true,
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
-    template: `
-        <div data-testid="counter-feature">Counter Stuff Goes Here</div>
-    `,
-    styles: ``
+  selector: 'app-counter',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink, RouterOutlet],
+  template: `
+    <div>
+      <a class="link" routerLink="ui">UI</a>
+      <router-outlet />
+    </div>
+  `,
+  styles: ``,
 })
-export class CounterComponent {
-
-}
+export class CounterComponent {}

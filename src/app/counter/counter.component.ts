1328029d8f74b103890aefb2a84749fae8b2eb0a
HT <Henry_Tremains@progressive.com>
2024-11-07 12:59:17 -0500

    create counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+    selector: 'app-counter',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+    template: `
+        <div data-testid="counter-feature">Counter Stuff Goes Here</div>
+    `,
+    styles: ``
+})
+export class CounterComponent {
+
+}
